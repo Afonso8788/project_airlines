2b8d23f6f29845a6f3a1f9093958847e76299bcc
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-16 22:28:51 +0000

    Commit

--- src/temporal_processing.py
+++ src/temporal_processing.py
@@ -4,5 +4,12 @@ def most_tweets_day(data):
         date = row["tweet created"].split('')[0]
         counts[date] = counts.get (date,0) + 1
         return max(counts,key= counts.get)
-
+def tweets_per_month(data , year , month):
+    count = 0
+    for row in data:
+        date = row['tweet created'].split('')[0]
+        y, m , _ = map (int , date.split('-'))
+        if y == year and m == month :
+            count +=1
+    return count
 
