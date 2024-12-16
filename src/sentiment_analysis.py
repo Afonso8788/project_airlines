8a132957b8c35c07391de699566afcb857f86f68
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-16 22:47:15 +0000

    Commit

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -17,8 +17,11 @@ def company_least_positive_tweets(data):
             counts[row['airline']] = counts.get(row['airline'], 0) + 1
     return min(counts, key=counts.get)
 def company_most_neutral_tweets(data):
-    neutral = data[data['airline_sentiment'] == 'neutral']
-    return neutral['airline'].value_counts().idxmax()
+    counts = {}
+    for row in data:
+        if row['airline_sentiment'] == 'neutral':
+            counts[row['airline']] = counts.get(row['airline'], 0) + 1
+    return max(counts, key=counts.get)
 def company_least_neutral_tweets(data):
     neutral = data[data['airline_sentiment'] == 'neutral']
     return neutral['airline'].value_counts().idxmin()
