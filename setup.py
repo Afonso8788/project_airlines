8db720696a1da582ba778e62a7a6e1887193da4d
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-17 16:28:14 +0000

    Commit

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='MyMath',
-    version='1.0',
-    description='MyMath Distribution Module',
-    author='Your Name',
-    author_email='Your Email',
-    url='The URL project - github for example',
-    py_modules=['MyMath'],
-    include_package_data=True
-     )
+setup(
+    name='project_airlines',
 
+    version='1.0.0',
+    author='Afonso Claro, Daniel Jimenez',
+    author_email='afonso.p.claro@ubi.pt, daniel.jimenez@ubi.pt',
+    description='Análise de tweets sobre companhias aéreas',
+    long_description=open('README.md.txt').read(),
+    url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo9',
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    python_requires='>=3.8'
+)
