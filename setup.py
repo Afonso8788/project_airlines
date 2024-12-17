6c08cf69f458ec6262e817554d390a2cd87c15ac
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-17 20:38:11 +0000

    Commit

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-from codecs import open
+
 from setuptools import setup, find_packages
-import os
-
-requirementPath = 'requirements.txt.txt'
-install_requires = []
-
-if os.path.isfile(requirementPath):
-    with open(requirementPath) as f:
-        install_requires = f.read().splitlines()
 
 setup(
     name="Project_Airlines",
@@ -15,7 +7,6 @@ setup(
     description="Project_Airlines Sentiment Package",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires,
     entry_points={
     },
     author='Afonso Claro'' Daniel Jimenez',
