c9b15fe8be3010fd1f8b8fe4dd13cc55d7983934
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-17 20:00:19 +0000

    Commit

---
+++ build/lib/src/data_reading.py
@@ -0,0 +1,8 @@
+import csv
+
+def path_arquive_csv(path_arquive):
+        with open(path_arquive, mode='r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                data = list(reader)
+        print(f"Arquivo '{path_arquive}' carregado com sucesso usando csv!")
+        return data
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(name='Project_airlines',
-      version='1.0',
+      version ='3.0',
       description='Project_airlines Distribution Module',
       author='Afonso Claro and Daniel Jimenez',
       author_email='afonso.p.claro@ubi.pt',
       url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo9',
       py_modules=['Project_airlines'],
+      python_requires='>=3.6'
      )
