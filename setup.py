035b618d58c5f8a560b44a97f133ce3f7786cb26
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-17 18:58:39 +0000

    Commit

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
+import os
+
 from setuptools import setup, find_packages
-
 setup(
-    name='project_airlines',
-
-    version='1.0.0',
-    author='Afonso Claro, Daniel Jimenez',
-    author_email='afonso.p.claro@ubi.pt, daniel.jimenez@ubi.pt',
-    description='Análise de tweets sobre companhias aéreas',
-    long_description=open('README.md.txt').read(),
-    url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo9',
-    packages=find_packages(where="src"),
-    package_dir={'': 'src'},
-    python_requires='>=3.8'
+    name="drawing",
+    version="0.1",
+    description="Drawing Distribution Package",
+    packages=find_packages(),
+    include_package_data=True
 )
