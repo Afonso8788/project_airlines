660f71d94e646df246fe9beeb59b5c842f46d3aa
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-17 20:30:39 +0000

    Commit

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from distutils.core import setup
-setup(name='Project_airlines',
-      version ='3.0',
-      description='Project_airlines Distribution Module',
-      author='Afonso Claro and Daniel Jimenez',
-      author_email='afonso.p.claro@ubi.pt',
-      url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo9',
-      py_modules=['Project_airlines'],
-      python_requires='>=3.6'
-     )
+from codecs import open
+from setuptools import setup, find_packages
+import os
+
+requirementPath = 'requirements.txt.txt'
+install_requires = []
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name="Project_Airlines",
+    version="0.1",
+    description="Project_Airlines Sentiment Package",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+    },
+    author='Afonso Claro'' Daniel Jimenez',
+    author_email='daniel.jimenez@ubi.pt''afonso.p.claro@ubi.pt',
+    url='https://github.com/ubi-datasciencelabs-students/IACD_Prog_20242025_Grupo9'
+)
