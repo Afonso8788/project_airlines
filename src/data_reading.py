086c38ecac775a54db2d7e313a662df6442594af
Afonso8788 <afonso.p.claro@ubi.pt>
2024-12-18 13:41:21 +0000

    Commit

--- src/data_reading.py
+++ src/data_reading.py
@@ -9,20 +9,3 @@ def path_arquive_csv(path_arquive):
         print(f"Arquivo '{path_arquive}' carregado com sucesso usando csv!")
         return data
 
-def configurate_logs(arquive_name="app.log"):
-    directory_logs = os.path.dirname(arquive_name)
-    if directory_logs and not os.path.exists(directory_logs):
-        os.makedirs(directory_logs)
-    logging.basicConfig(
-        filename=arquive_name,
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    )
-    logger = logging.getLogger()
-    logger.info("Sistema de logs configurado.")
-    return logger
-def register_event(logger, message):
-    logger.info(message)
-def register_error(logger, message):
-    logger.error(message)
